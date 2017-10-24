49dc20a3d5e621ca4393f02f69ef3764007f52a2
Shuntaro Takasaki <s_tksk@nttpc.co.jp>
2017-10-24 13:06:35 +0000

    20171024

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 100.times do |n|
   email = Faker::Internet.email
   password = "password"
-#  name = Faker::FunnyName.name
+  name = Faker::FunnyName.name
   User.create!(email: email,
-#               name: name,
+               name: name,
                password: password,
                password_confirmation: password,
                )

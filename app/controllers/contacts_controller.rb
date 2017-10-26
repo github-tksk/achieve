5b37610027b2646fa4f7095e5e60380b79b9d4db
Shuntaro Takasaki <s_tksk@nttpc.co.jp>
2017-10-26 10:05:18 +0000

    dive11

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,8 @@ class ContactsController < ApplicationController
     Contact.create(contacts_params)
     @contact = Contact.new(contacts_params)
     if @contact.save
-    redirect_to root_path, notice: "お問い合わせを受け付けました"
+      redirect_to root_path, notice: "お問い合わせを受け付けました"
+      NoticeMailer.sendmail_contact(@contact).deliver
     else
       # 入力フォームを再描画します。
       render 'new'
